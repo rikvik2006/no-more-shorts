b9d954c3db33ea65e0cf3d3a37684191758540cc
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-11-27 15:55:43 +0100

    feat: Setuppped project with React Typescript and Rollup

---
+++ src/pages/popup/App.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { FC } from "react"
+
+const App: FC = (): JSX.Element => {
+    return (
+        <div>
+            <h1>Popup Page</h1>
+            <p>It works</p>
+        </div>
+    )
+}
+
+export default App
